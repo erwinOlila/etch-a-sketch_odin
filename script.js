1804749f57d7d4816aecd42bf3d079c03a86ff9c
erwinOlila <ce.olila@gmail.com>
2022-06-25 14:18:11 +0800

    completed sketch with colors

--- script.js
+++ script.js
@@ -4,25 +4,62 @@ function makeDivs(h, u) {
     square.style.height = `${h/u}`;
     square.textContent = " ";
     square.addEventListener('mouseenter', function(e) {
-        e.target.style.backgroundColor = 'black';
+        e.target.style.backgroundColor = colorful ? `rgba(${rndm(255)},${rndm(255)},${rndm(255)},1)` : erase ? 'rgba(255, 255, 255, 1)' : 'rgba(0, 0, 0, 1)';
     })
 
     return square; 
 }
 
 
-const width = 720;
-const height = 720;
+let counter = 10;
+let colorful = false;
+let erase = false;
 
 const container = document.querySelector('.container');
 container.gridGap = '0';
 container.style.border = '1px solid black';
 
+const width = container.clientWidth + 1;
+const height = container.clientHeight + 1;
+
 const slider = document.querySelector('#slider');
 const sliderValue = document.querySelector('#sliderValue');
 sliderValue.textContent = slider.value;
 
+const colors = document.querySelectorAll('button:not(#clear)');
+const clear = document.querySelector('#clear');
+
+colors.forEach(color => {
+    if (color.id === 'black') {
+        color.classList.add('hover');
+    }
+    color.addEventListener('click', setColor);
+})
+
+function setColor() {
+    colors.forEach(color => {
+        color.classList.remove('hover');
+    });
+
+    this.classList.add('hover');
+    if (this.id === 'colorful') {
+        colorful = true;
+    } else {
+        colorful = false;
+        if (this.id === 'erase') {
+            erase = true;
+        } else {
+            erase = false;
+        }
+    }
+}
+
+function rndm(max) {
+    return Math.floor((Math.random() * max) + 1);
+}
+
 slider.addEventListener('input', setSquares);
+clear.addEventListener('click', setSquares);
 let counts = slider.value;
 
 container.style.gridTemplateColumns = `repeat(${counts}, ${width/counts}px)`;
@@ -32,8 +69,14 @@ let firstDivs = Array(Math.pow(counts,2)).fill().map(function() {
 container.replaceChildren(...firstDivs); 
 container.style.gridTemplateColumns = `repeat(${counts}, ${width/counts}px)`;
 
-function setSquares(e) {
-    const value = this.value;
+function setSquares() {
+    let value = 0;
+    if(this.value) {
+        value = this.value;
+    } else {
+        value = slider.value;
+    }
+    
 
     let divs_array = Array(Math.pow(value,2)).fill().map(function() {
         return makeDivs(height, value);
@@ -41,7 +84,7 @@ function setSquares(e) {
 
     container.replaceChildren(...divs_array);
     container.style.gridTemplateColumns = `repeat(${value}, ${width/value}px)`;
-    sliderValue.textContent = this.value;
+    sliderValue.textContent = value;
 }
 
 
